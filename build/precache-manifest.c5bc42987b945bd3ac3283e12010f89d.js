ce327f42c3e2924d0a954791cee169867918f89c
vinaykumarck427 <vinaykumarck427@gmail.com>
2020-06-05 00:52:45 +0530

    second commit

---
+++ build/precache-manifest.c5bc42987b945bd3ac3283e12010f89d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f93e929a6b74ca3e08122c52ca5813",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "11df9a35e76d16493fd8",
+    "url": "/react-deploy/static/css/main.b312a01f.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11df9a35e76d16493fd8",
+    "url": "/react-deploy/static/js/main.f3319577.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  },
+  {
+    "revision": "e31576836cef3f7d9ad1b1dfc6947a5f",
+    "url": "/react-deploy/static/media/my_image.e3157683.jpg"
+  }
+]);
